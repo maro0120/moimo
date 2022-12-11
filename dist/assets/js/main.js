daaf3974f2a696716fd30ee185d5492589c82459
maro0120 <kmanabe0120@gmail.com>
2022-12-11 23:27:19 +0900

    add top

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,102 +1,98 @@
 // 読み込み後アニメーション
-window.addEventListener("load",function(){
-  const LoadMask = document.getElementById('load__mask');
+window.addEventListener("load", function () {
+  const LoadMask = document.getElementById("load__mask");
 
   setTimeout(function () {
-    LoadMask.classList.add('reval');
-  },500);
+    LoadMask.classList.add("reval");
+  }, 500);
 
-  
-  const LoaderImgs = document.querySelectorAll('.loader__img');
-  
-  LoaderImgs.forEach(function( LoaderImg,index){
-    setTimeout(function(){
-      LoaderImg.classList.add('js-animated');
+  const LoaderImgs = document.querySelectorAll(".loader__img");
+
+  LoaderImgs.forEach(function (LoaderImg, index) {
+    setTimeout(function () {
+      LoaderImg.classList.add("js-animated");
     }, 1100 + 300 * (index + 1));
   });
 
+  const JsLoader = document.getElementById("js__loader");
+  const LoaderImg05 = document.querySelectorAll(".loader__img05");
+  const body = document.querySelector("body");
 
-  const JsLoader = document.getElementById('js__loader');
-  const LoaderImg05 = document.querySelectorAll('.loader__img05');
-  const body = document.querySelector('body');
-
-  setTimeout(function(){
-    JsLoader.classList.add('js-fadeOut');
-    body.classList.remove('over-hidden');
+  setTimeout(function () {
+    JsLoader.classList.add("js-fadeOut");
+    body.classList.remove("over-hidden");
   }, 4000);
 
-
   const tl = gsap.timeline();
-  const fvImg = document.querySelectorAll('.fv__img');
-  const jsFvImg = document.getElementById('js-fv-img');
-  const jsFvSpan01 = document.querySelectorAll('.fv-span-01');
-  const jsFvSpan02 = document.querySelectorAll('.fv-span-02');
-  const jsFvSpan03 = document.querySelectorAll('.fv-span-03');
-  const jsHeader = document.getElementById('js-header');
+  const fvImg = document.querySelectorAll(".fv__img");
+  const jsFvImg = document.getElementById("js-fv-img");
+  const jsFvSpan01 = document.querySelectorAll(".fv-span-01");
+  const jsFvSpan02 = document.querySelectorAll(".fv-span-02");
+  const jsFvSpan03 = document.querySelectorAll(".fv-span-03");
+  const jsHeader = document.getElementById("js-header");
 
   tl.to(
     fvImg,
     {
-      y:0,
-      opacity:1,
+      y: 0,
+      opacity: 1,
     },
     5
   )
 
-  .to(
-    jsFvImg,
-    {
-      scale: 1,
-      opacity:1,
-    },
-    5
-  )
+    .to(
+      jsFvImg,
+      {
+        scale: 1,
+        opacity: 1,
+      },
+      5
+    )
 
-  .to(
-    jsFvSpan01,
-    {
-      y:0,
-      opacity: 1,
-      ease: Power4.out,
-    },
-    "-=.5"
-  )
+    .to(
+      jsFvSpan01,
+      {
+        y: 0,
+        opacity: 1,
+        ease: Power4.out,
+      },
+      "-=.5"
+    )
 
-  .to(
-    jsFvSpan02,
-    {
-      y:0,
-      opacity: 1,
-      ease: Power4.out,
-    },
-    "-=0.4"
-  )
+    .to(
+      jsFvSpan02,
+      {
+        y: 0,
+        opacity: 1,
+        ease: Power4.out,
+      },
+      "-=0.4"
+    )
 
-  .to(
-    jsFvSpan03,
-    {
-      y:0,
-      opacity: 1,
-      ease: Power4.out,
-    },
-    "-=0.4"
-  )
+    .to(
+      jsFvSpan03,
+      {
+        y: 0,
+        opacity: 1,
+        ease: Power4.out,
+      },
+      "-=0.4"
+    )
 
-  .to(
-    jsHeader,
-    {
-      y:0,
-      opacity:1,
-      ease: Circ.out,
-    },
-    "+=.2"
-  )
-
+    .to(
+      jsHeader,
+      {
+        y: 0,
+        opacity: 1,
+        ease: Circ.out,
+      },
+      "+=.2"
+    );
 });
 
 //locomotiveScroll
 const locoScroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
+  el: document.querySelector("[data-scroll-container]"),
   smooth: true,
 });
 
@@ -123,265 +119,249 @@ ScrollTrigger.scrollerProxy(".site__main__contents", {
     : "fixed",
 });
 
-
 // スクロールに合わせて画像を表示させる
 gsap.registerPlugin(ScrollTrigger);
 
 // messageの画像
-const messageImgs = document.querySelectorAll('.js-message__img');
+const messageImgs = document.querySelectorAll(".js-message__img");
 
 messageImgs.forEach((messageImg, index) => {
-  gsap.to(
-    messageImg,
-    {
-      scrollTrigger: {
-        trigger: messageImg,
-        start: 'top bottom-=300',
-        end: 'top top',
-        once: true,
-        scroller: ".site__main__contents",
-        onEnter: () => messageImg.classList.add("js-fadeImg"),
-      }
-    }
-  );
+  gsap.to(messageImg, {
+    scrollTrigger: {
+      trigger: messageImg,
+      start: "top bottom-=300",
+      end: "top top",
+      once: true,
+      scroller: ".site__main__contents",
+      onEnter: () => messageImg.classList.add("js-fadeImg"),
+    },
+  });
 });
 
 // messageのタイトル
-const jsFadeUpTitles = document.querySelectorAll('.fade-up-ttl');
+const jsFadeUpTitles = document.querySelectorAll(".fade-up-ttl");
 
 jsFadeUpTitles.forEach((jsFadeUpTitle, index) => {
-  gsap.to(
-    jsFadeUpTitle,
-  {
+  gsap.to(jsFadeUpTitle, {
     scrollTrigger: {
       trigger: jsFadeUpTitle,
-      start: 'top-=100 bottom',
-      end: 'top top',
+      start: "top-=100 bottom",
+      end: "top top",
       once: true,
       scroller: ".site__main__contents",
       onEnter: () => jsFadeUpTitle.classList.add("js-fadeUp-ttl"),
-    }
+    },
   });
 });
 
 // セクション間の横線
-const jsBorders = document.querySelectorAll('.border');
+const jsBorders = document.querySelectorAll(".border");
 
 jsBorders.forEach((jsBorder, index) => {
-  gsap.to(
-    jsBorder,
-  {
+  gsap.to(jsBorder, {
     scrollTrigger: {
       trigger: jsBorder,
-      start: 'top center',
-      end: 'top top',
+      start: "top center",
+      end: "top top",
       once: true,
       scroller: ".site__main__contents",
       onEnter: () => jsBorder.classList.add("jsBorder"),
-    }
+    },
   });
 });
 
 // タイトルが3Dっぽくフェードアップ
-const jsFadeUpTitle02s = document.querySelectorAll('.fade-up-ttl-02');
+const jsFadeUpTitle02s = document.querySelectorAll(".fade-up-ttl-02");
 
 jsFadeUpTitle02s.forEach((jsFadeUpTitle02, index) => {
-  gsap.to(
-    jsFadeUpTitle02,
-  {
+  gsap.to(jsFadeUpTitle02, {
     scrollTrigger: {
       trigger: jsFadeUpTitle02,
-      start: 'top bottom',
-      end: 'top top',
+      start: "top bottom",
+      end: "top top",
       once: true,
       scroller: ".site__main__contents",
       onEnter: () => jsFadeUpTitle02.classList.add("js-fadeUp-ttl-02"),
-    }
+    },
   });
 });
 
-
 // 画像がスクロールに合わせて表示されて消えていく
-const ProductImgs = document.querySelectorAll('.product__img');
+const ProductImgs = document.querySelectorAll(".product__img");
 
 ProductImgs.forEach((ProductImg, index) => {
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ProductImg,
-      start: 'top bottom',
-      end:'bottom top',
-      scrub: 1.2,
-      scroller: ".site__main__contents",
-    }
-  })
-  .to(ProductImg,{
-    opacity: -0.2,
-    y: 150,
-  })
-  .to(ProductImg,{
-    opacity: 0.8,
-    y: 0,
-  })
-  .to(ProductImg,{
-    opacity: -0.2,
-    y: -150,
-  })
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ProductImg,
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1.2,
+        scroller: ".site__main__contents",
+      },
+    })
+    .to(ProductImg, {
+      opacity: -0.2,
+      y: 150,
+    })
+    .to(ProductImg, {
+      opacity: 0.8,
+      y: 0,
+    })
+    .to(ProductImg, {
+      opacity: -0.2,
+      y: -150,
+    });
 });
 
-
 //スクロールに合わせて縦に線が伸びていく
-const Works = document.getElementById('works');
-const WorksItem06 = document.getElementById('works__item__06');
-const WorksBar = document.querySelectorAll('.works__scroll__bar');
+const Works = document.getElementById("works");
+const WorksItem06 = document.getElementById("works__item__06");
+const WorksBar = document.querySelectorAll(".works__scroll__bar");
 
 gsap.fromTo(
-  WorksBar, {
-    height: '0vh',
+  WorksBar,
+  {
+    height: "0vh",
   },
   {
-    height: '100vh',
+    height: "100vh",
     scrollTrigger: {
       trigger: Works,
-      start: 'top top',
+      start: "top top",
       endTrigger: WorksItem06,
-      end: 'bottom bottom',
+      end: "bottom bottom",
       scrub: 1,
       scroller: ".site__main__contents",
-    }
+    },
   }
-)
+);
 
 //背景がスクロールに合わせて変わっていく
 ScrollTrigger.create({
-  trigger: '#works__item__02',
-  start: 'top top',
-  endTrigger: '#works__item__03',
-  end: 'center center',
+  trigger: "#works__item__02",
+  start: "top top",
+  endTrigger: "#works__item__03",
+  end: "center center",
   scroller: ".site__main__contents",
   toggleClass: {
     targets: Works,
-    className: 'js-bg-change01',
-  }
-})
+    className: "js-bg-change01",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__03',
-  start: 'top top',
-  endTrigger: '#works__item__04',
-  end: 'center center',
+  trigger: "#works__item__03",
+  start: "top top",
+  endTrigger: "#works__item__04",
+  end: "center center",
   scroller: ".site__main__contents",
   toggleClass: {
     targets: Works,
-    className: 'js-bg-change02',
-  }
-})
+    className: "js-bg-change02",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__04',
-  start: 'top top',
-  endTrigger: '#works__item__05',
-  end: 'center center',
+  trigger: "#works__item__04",
+  start: "top top",
+  endTrigger: "#works__item__05",
+  end: "center center",
   scroller: ".site__main__contents",
   toggleClass: {
     targets: Works,
-    className: 'js-bg-change03',
-  }
-})
+    className: "js-bg-change03",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__05',
-  start: 'top top',
-  endTrigger: '#works__item__06',
-  end: 'center center',
+  trigger: "#works__item__05",
+  start: "top top",
+  endTrigger: "#works__item__06",
+  end: "center center",
   scroller: ".site__main__contents",
   toggleClass: {
     targets: Works,
-    className: 'js-bg-change04',
-  }
-})
+    className: "js-bg-change04",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__06',
-  start: 'top top',
-  end: 'bottom+=100% bottom',
+  trigger: "#works__item__06",
+  start: "top top",
+  end: "bottom+=100% bottom",
   scroller: ".site__main__contents",
   toggleClass: {
     targets: Works,
-    className: 'js-bg-change05',
-  }
-})
+    className: "js-bg-change05",
+  },
+});
 //固定してテキストと画像がスクロールに合わせて入れ替わっていく
-const WorksRightList01 = document.getElementById('works__right__list1');
-const WorksRightList02 = document.getElementById('works__right__list2');
-const WorksRightList03 = document.getElementById('works__right__list3');
-const WorksRightList04 = document.getElementById('works__right__list4');
-const WorksRightList05 = document.getElementById('works__right__list5');
-const WorksRightList06 = document.getElementById('works__right__list6');
+const WorksRightList01 = document.getElementById("works__right__list1");
+const WorksRightList02 = document.getElementById("works__right__list2");
+const WorksRightList03 = document.getElementById("works__right__list3");
+const WorksRightList04 = document.getElementById("works__right__list4");
+const WorksRightList05 = document.getElementById("works__right__list5");
+const WorksRightList06 = document.getElementById("works__right__list6");
 
 ScrollTrigger.create({
-  trigger: '#works__item__01',
-  start: 'top top',
-  endTrigger: '#works__item__02',
-  end:'center center',
+  trigger: "#works__item__01",
+  start: "top top",
+  endTrigger: "#works__item__02",
+  end: "center center",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList01,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__02',
-  start: 'top top',
-  endTrigger: '#works__item__03',
-  end:'center center',
+  trigger: "#works__item__02",
+  start: "top top",
+  endTrigger: "#works__item__03",
+  end: "center center",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList02,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__03',
-  start: 'top top',
-  endTrigger: '#works__item__04',
-  end:'center center',
+  trigger: "#works__item__03",
+  start: "top top",
+  endTrigger: "#works__item__04",
+  end: "center center",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList03,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__04',
-  start: 'top top',
-  endTrigger: '#works__item__05',
-  end:'center center',
+  trigger: "#works__item__04",
+  start: "top top",
+  endTrigger: "#works__item__05",
+  end: "center center",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList04,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__05',
-  start: 'top top',
-  endTrigger: '#works__item__06',
-  end:'center center',
+  trigger: "#works__item__05",
+  start: "top top",
+  endTrigger: "#works__item__06",
+  end: "center center",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList05,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
 ScrollTrigger.create({
-  trigger: '#works__item__06',
-  start: 'top top',
-  end:'bottom+=100% bottom',
+  trigger: "#works__item__06",
+  start: "top top",
+  end: "bottom+=100% bottom",
   scroller: ".site__main__contents",
-  toggleClass:{
+  toggleClass: {
     targets: WorksRightList06,
-    className: 'js-active',
-  }
-  }
-)
+    className: "js-active",
+  },
+});
---
+++ src/main.js
@@ -0,0 +1,239 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+  easing: "easeOutQuint",
+  speed: 1000,
+  header: "[data-scroll-header]",
+});
+jQuery(document).ready(function ($) {
+  $(".toggle-btn").on("click", function () {
+    $(".toggle-btn__line").toggleClass("active");
+    $(".global-nav").fadeToggle();
+  });
+});
+jQuery(document).ready(function ($) {
+  $(".global-nav__item").on("click", function () {
+    $(".toggle-btn__line").toggleClass("active");
+    $(".global-nav").fadeToggle();
+  });
+});
+const svg = document.querySelector("#svg");
+const img = document.querySelector("#img");
+const circle = document.querySelector("#circle");
+const content = document.querySelector("#content");
+const whiteLayer = document.querySelector("#whiteLayer");
+const pad = 4;
+let radius = +circle.getAttribute("r");
+let imgWidth, imgHeight;
+const tl = gsap.timeline();
+tl.to(circle, {
+  attr: {
+    r: () => radius,
+  },
+  delay: 0.5,
+  ease: "power1.in",
+})
+  .to(
+    "#whiteLayer",
+    {
+      alpha: 0,
+      ease: "power1.in",
+      duration: 1 - 0.25,
+      opacity: 0,
+    },
+    0.25
+  )
+  .to(svg, {
+    display: "none",
+  });
+init();
+
+function init() {
+  imgWidth = img.naturalWidth;
+  imgHeight = img.naturalHeight;
+  resize();
+}
+
+function resize() {
+  tl.progress(0);
+  const r = svg.getBoundingClientRect();
+  const rectWidth = r.width + pad;
+  const rectHeight = r.height + pad;
+  const rx = rectWidth / imgWidth;
+  const ry = rectHeight / imgHeight;
+  const ratio = Math.max(rx, ry);
+  const width = imgWidth * ratio;
+  const height = imgHeight * ratio;
+  const dx = rectWidth / 2;
+  const dy = rectHeight / 2;
+  radius = Math.sqrt(dx * dx + dy * dy);
+  gsap.set(img, { width, height });
+}
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("li.l-gnav__item").forEach((element) => {
+    // 親要素に data-overwrite 属性が適用されているかで判断
+    const useFlag = element.parentElement.dataset.overwrite === "true";
+    const overwrite = useFlag ? "auto" : false;
+    element.addEventListener("mouseenter", () => {
+      gsap.to(element, {
+        scale: 1.2,
+        duration: 0.5,
+        overwrite,
+      });
+    });
+    element.addEventListener("mouseleave", () => {
+      gsap.to(element, {
+        scale: 1.0,
+        duration: 0.5,
+        overwrite,
+      });
+    });
+  });
+});
+const myDelay = 7000;
+let timer;
+const switchAnimation = () => {
+  clearTimeout(timer);
+  let activeSlide = document.querySelectorAll(".swiper-slide[class*=-active]");
+  for (let i = 0; i < activeSlide.length; i++) {
+    activeSlide[i].classList.remove("anm-finished");
+    activeSlide[i].classList.add("anm-started");
+  }
+  timer = setTimeout(() => {
+    for (let i = 0; i < activeSlide.length; i++) {
+      activeSlide[i].classList.remove("anm-started");
+      activeSlide[i].classList.add("anm-finished");
+    }
+  }, myDelay - 1000);
+};
+const finishAnimation = () => {
+  let activeSlide = document.querySelectorAll(".swiper-slide.anm-started");
+  for (let i = 0; i < activeSlide.length; i++) {
+    activeSlide[i].classList.remove("anm-started");
+    activeSlide[i].classList.add("anm-finished");
+  }
+};
+const mySwiper = new Swiper(".swiper", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  loop: true,
+  loopAdditionalSlides: 1,
+  speed: 2000,
+  autoplay: {
+    delay: myDelay,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  followFinger: false,
+  on: {
+    slideChange: () => {
+      finishAnimation();
+    },
+    slideChangeTransitionStart: () => {
+      switchAnimation();
+    },
+  },
+});
+//Conceptアニメーション
+const conceptImg1 = document.querySelector("#conceptImg1");
+const conceptTxt1_1 = document.querySelector("#conceptTxt1_1");
+const conceptTxt1_2 = document.querySelector("#conceptTxt1_2");
+gsap.set(conceptImg1, {
+  autoAlpha: 0, //ここで初期状態を設定
+  xPercent: -100,
+  filter: "grayscale(100%)",
+});
+gsap.set([conceptTxt1_1, conceptTxt1_2], {
+  autoAlpha: 0, //ここで初期状態を設定
+  yPercent: -100,
+});
+const tlConcept = gsap.timeline({
+  scrollTrigger: {
+    trigger: conceptImg1, //アニメーションが始まるトリガーとなる要素
+    start: "top bottom",
+    markers: true,
+  },
+});
+tlConcept
+  .to(conceptImg1, {
+    autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+    xPercent: 0,
+    filter: "grayscale(0%)",
+    duration: 1,
+  })
+  .to(
+    [conceptTxt1_1, conceptTxt1_2],
+    {
+      autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+      yPercent: 0,
+    },
+    "<"
+  );
+const conceptImg2 = document.querySelector("#conceptImg2");
+const conceptTxt2_1 = document.querySelector("#conceptTxt2_1");
+const conceptTxt2_2 = document.querySelector("#conceptTxt2_2");
+gsap.set(conceptImg2, {
+  autoAlpha: 0, //ここで初期状態を設定
+  xPercent: 100,
+  filter: "grayscale(100%)",
+});
+gsap.set([conceptTxt2_1, conceptTxt2_2], {
+  autoAlpha: 0, //ここで初期状態を設定
+  yPercent: -100,
+});
+const tlConcept2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: conceptImg2, //アニメーションが始まるトリガーとなる要素
+    start: "top bottom",
+    markers: true,
+  },
+});
+tlConcept2
+  .to(conceptImg2, {
+    autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+    xPercent: 0,
+    filter: "grayscale(0%)",
+    duration: 1,
+  })
+  .to(
+    [conceptTxt2_1, conceptTxt2_2],
+    {
+      autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+      yPercent: 0,
+    },
+    "<"
+  );
+const conceptImg3 = document.querySelector("#conceptImg3");
+const conceptTxt3_1 = document.querySelector("#conceptTxt3_1");
+const conceptTxt3_2 = document.querySelector("#conceptTxt3_2");
+gsap.set(conceptImg3, {
+  autoAlpha: 0, //ここで初期状態を設定
+  xPercent: -100,
+  filter: "grayscale(100%)",
+});
+gsap.set([conceptTxt3_1, conceptTxt3_2], {
+  autoAlpha: 0, //ここで初期状態を設定
+  yPercent: -100,
+});
+const tlConcept3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: conceptImg3, //アニメーションが始まるトリガーとなる要素
+    start: "top bottom",
+    markers: true,
+  },
+});
+tlConcept3
+  .to(conceptImg3, {
+    autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+    xPercent: 0,
+    filter: "grayscale(0%)",
+    duration: 1,
+  })
+  .to(
+    [conceptTxt3_1, conceptTxt3_2],
+    {
+      autoAlpha: 1, //ここでアニメーションさせたい内容を書く
+      yPercent: 0,
+    },
+    "<"
+  );
